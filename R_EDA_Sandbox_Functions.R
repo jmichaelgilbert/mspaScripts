ab63c5e9989baca6d53d5e1cbaa74d2d02bcf8ff
Michael Gilbert <michael.gilbert@leaf.state.va.us>
2016-08-02 14:04:56 -0400

    Updated code from R_EDA_Sandbox

--- R_EDA_Sandbox_Functions.R
+++ R_EDA_Sandbox_Functions.R
@@ -1,6 +1,6 @@
 ###############################################################################
 # R_EDA_Sandbox_Functions.R
-# Last updated: 2016-07-16 by MJG
+# Last updated: 2016-08-02 by MJG
 ###############################################################################
 
 # A compilation of useful functions to [ideally] deploy on any data set
@@ -41,14 +41,16 @@ fit = function(f, x){
 # miss.flag()
 #--------------------------------------
 # Function to create indicator variables as missing flags
-miss.flag = function(df, cn = c("num", "fac")){
-    if (missing(cn)){
+miss.flag = function(df, df.cn = c("num", "fac")){
+    # Check for columns to apply
+    if (missing(df.cn)){
         cols = colnames(df)
-    } else if (cn == "num"){
+    } else if (df.cn == "num"){
         cols = colnames(df[, !sapply(df, is.factor)])
-    } else if (cn == "fac"){
+    } else if (df.cn == "fac"){
         cols = colnames(df[, sapply(df, is.factor)])
     }
+    # Apply function
     for (i in cols){
         if (sum(is.na(df[, i])) > 0){
             df[paste("MF", i, sep = "_")] =
@@ -71,8 +73,21 @@ miss.flag = function(df, cn = c("num", "fac")){
 #--------------------------------------
 # Function to create boxplots of numeric variables
 num.boxplot = function(df, df.fac){
-    df.name = deparse(substitute(df))
-    cols = colnames(df[, !sapply(df, is.factor)])
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("numeric", "integer")){
+            stop("Please supply a numeric or integer variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        df.name = temp[1]
+        cols = temp[2]
+    } else {
+        df.name = deparse(substitute(df))
+        cols = colnames(df[, !sapply(df, is.factor)])
+    }
+    # Create plot(s)
     for (i in cols){
         if (missing(df.fac)){
             boxplot(df[, i], col = "grey", horizontal = T,
@@ -96,10 +111,23 @@ num.boxplot = function(df, df.fac){
 # num.hist()
 #--------------------------------------
 # Function to create histograms of numeric variables
-# Optional choice of normal curve overlay
+# Optional choice of equal spaced probabilities or normal curve overlay
 num.hist = function(df, prob = F, norm = T){
-    df.name = deparse(substitute(df))
-    cols = colnames(df[, !sapply(df, is.factor)])
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("numeric", "integer")){
+            stop("Please supply a numeric or integer variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        df.name = temp[1]
+        cols = temp[2]
+    } else {
+        df.name = deparse(substitute(df))
+        cols = colnames(df[, !sapply(df, is.factor)])
+    }
+    # Create plot(s)
     for (i in cols){
         main = paste("Histogram of ", df.name, "$", i, sep = "")
         sub = ifelse(norm, "normal curve overlay (blue)", "")
@@ -112,7 +140,7 @@ num.hist = function(df, prob = F, norm = T){
         }
         if (!prob){
             h = hist(df[, i], col = "grey", main = main, sub = sub,
-                     ylim = c(0, 1.1*max(y$counts)),
+                     ylim = c(0, 1.15*max(y$counts)),
                      xlab = paste(df.name, "$", i, sep = ""))
         }
         if (norm){
@@ -131,8 +159,21 @@ num.hist = function(df, prob = F, norm = T){
 #--------------------------------------
 # Function to create Q-Q plots of numeric variables
 num.qq = function(df){
-    df.name = deparse(substitute(df))
-    cols = colnames(df[, !sapply(df, is.factor)])
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("numeric", "integer")){
+            stop("Please supply a numeric or integer variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        df.name = temp[1]
+        cols = temp[2]
+    } else {
+        df.name = deparse(substitute(df))
+        cols = colnames(df[, !sapply(df, is.factor)])
+    }
+    # Create plot(s)
     for (i in cols){
         qqnorm(df[, i], pch = 21, bg = "grey",
                main = paste("Normal Q-Q Plot of ", df.name, "$", i, sep = ""))
@@ -145,10 +186,23 @@ num.qq = function(df){
 #--------------------------------------
 # Function to create scatterplots of numeric variables
 num.scatter = function(df, df.num){
-    df.name = deparse(substitute(df))
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("numeric", "integer")){
+            stop("Please supply a numeric or integer variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        df.name = temp[1]
+        cols = temp[2]
+    } else {
+        df.name = deparse(substitute(df))
+        cols = colnames(df[, !sapply(df, is.factor)])
+    }
+    # Create plot(s)
     num = unlist(strsplit(deparse(substitute(df.num)),
                           split = "$", fixed = T))[2]
-    cols = colnames(df[, !sapply(df, is.factor)])
     for (i in cols){
         plot(df[, i], df[, num], pch = 21, bg = "grey",
              main = paste(df.name, "$", num, " versus ",
@@ -161,9 +215,6 @@ num.scatter = function(df, df.num){
 #--------------------------------------
 # num.plots()
 #--------------------------------------
-#--------------------------------------
-# num.plots()
-#--------------------------------------
 # Function to produce four plots per variable:
 # num.plots(which = ) corresponds as follows:
 #   1 = Histogram
@@ -172,11 +223,25 @@ num.scatter = function(df, df.num){
 #   4 = QQ Plot
 num.plots = function(df, df.num, df.fac, prob = F, norm = T,
                      which = c(1, 2, 3, 4)){
+    # Check for which plots to create
     if (missing(which)){
         par(mfcol = c(2, 2))
     }
-    df.name = deparse(substitute(df))
-    cols = colnames(df[, !sapply(df, is.factor)])
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("numeric", "integer")){
+            stop("Please supply a numeric or integer variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        df.name = temp[1]
+        cols = temp[2]
+    } else {
+        df.name = deparse(substitute(df))
+        cols = colnames(df[, !sapply(df, is.factor)])
+    }
+    # Create plot(s)
     for (i in cols){
         #------------------------------
         # Histograms
@@ -193,7 +258,7 @@ num.plots = function(df, df.num, df.fac, prob = F, norm = T,
             }
             if (!prob){
                 h = hist(df[, i], col = "grey", main = main, sub = sub,
-                         ylim = c(0, 1.1*max(y$counts)),
+                         ylim = c(0, 1.15*max(y$counts)),
                          xlab = paste(df.name, "$", i, sep = ""))
             }
             if (norm){
@@ -257,21 +322,23 @@ num.plots = function(df, df.num, df.fac, prob = F, norm = T,
 # num.freq()
 #--------------------------------------
 # Summary statistics split by named factor for numeric variables
-num.freq = function(df.fac, df.cn){
+num.freq = function(df.fac, df.num.cn){
+    table.results = data.frame()
+    # Check df.fac is factor
     if (!class(df.fac) %in% c("factor")){
         stop("Please supply a factor variable to df.fac")
     }
-    table.results = data.frame()
-    df = eval(as.name(paste(unlist(strsplit(deparse(substitute(df.fac)),
-                            split = "$", fixed = T))[1])))
-    fac = unlist(strsplit(deparse(substitute(df.fac)),
-                          split = "$", fixed = T))[2]
-    if (missing(df.cn)){
+    # Assign data.frame and name
+    temp = unlist(strsplit(deparse(substitute(df.fac)),
+                           split = "$", fixed = T))
+    df = eval(as.name(paste(temp[1])))
+    fac = temp[2]
+    if (missing(df.num.cn)){
         cols = colnames(df[, !sapply(df, is.factor)])
-    } else if (!class(df.cn) %in% c("numeric", "integer")){
-        stop("Please supply a numeric or integer variable to df.cn")
+    } else if (!class(df.num.cn) %in% c("numeric", "integer")){
+        stop("Please supply a numeric or integer variable to df.num.cn")
     } else {
-        cols = unlist(strsplit(deparse(substitute(df.cn)),
+        cols = unlist(strsplit(deparse(substitute(df.num.cn)),
                                split = "$", fixed = T))[2]
     }
     for (i in cols){
@@ -295,7 +362,19 @@ num.freq = function(df.fac, df.cn){
 #--------------------------------------
 # Function to scale (normalize: mean = 0, sd = 1) numeric variables
 num.scale = function(df){
-    cols = colnames(df[, !sapply(df, is.factor)])
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("numeric", "integer")){
+            stop("Please supply a numeric or integer variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        cols = temp[2]
+    } else {
+        cols = colnames(df[, !sapply(df, is.factor)])
+    }
+    # Apply function
     for (i in cols){
         i_cs = paste(i, "cs", sep = "_")
         df[i_cs] = scale(df[, i])
@@ -308,7 +387,19 @@ num.scale = function(df){
 #--------------------------------------
 # Function to transform numeric variables
 num.trans = function(df){
-    cols = colnames(df[, !sapply(df, is.factor)])
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("numeric", "integer")){
+            stop("Please supply a numeric or integer variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        cols = temp[2]
+    } else {
+        cols = colnames(df[, !sapply(df, is.factor)])
+    }
+    # Apply function
     for (i in cols){
         # Natural Log
         i_ln = paste(i, "ln", sep = "_")
@@ -329,7 +420,19 @@ num.trans = function(df){
 # Function to trim numeric variables at various percentiles
 num.trims = function(df){
     require(scales)
-    cols = colnames(df[, !sapply(df, is.factor)])
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("numeric", "integer")){
+            stop("Please supply a numeric or integer variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        cols = temp[2]
+    } else {
+        cols = colnames(df[, !sapply(df, is.factor)])
+    }
+    # Apply function
     for (i in cols){
         # 1st and 99th
         T99 = quantile(df[, i], c(0.01, 0.99))
@@ -356,34 +459,41 @@ num.trims = function(df){
 #------------------------------------------------------------------------------
 
 #--------------------------------------
-# fac.boxplot()
-#--------------------------------------
-fac.boxplot = function(df, list, var){
-    for (num in list){
-        boxplot(df[, var] ~ df[, num], col = "grey",
-                main = paste(data.name, num," versus ",
-                             data.name, var, sep = ""),
-                ylab = "Values")
-    }
-}
-
-#--------------------------------------
 # fac.barplot()
 #--------------------------------------
 # Function to create barplots of factor variables
-fac.barplot = function(df, list, var = NULL, cat = F){
-    for (fac in list){
-        if (cat){
-            barplot(table(df[, var], df[, fac]),
-                    main = paste("Variable: ", data.name, fac, sep = ""),
-                    ylim = c(0, 1.1*max(summary(df[, fac]))),
-                    ylab = "Frequency", beside = T)
+fac.barplot = function(df, df.num){
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("factor")){
+            stop("Please supply a factor variable to df")
         }
-        if (!cat){
-            plot(df[, fac],
-                 main = paste("Variable: ", data.name, fac, sep = ""),
-                 ylim = c(0, 1.1*max(summary(df[, fac]))),
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        df.name = temp[1]
+        cols = temp[2]
+    } else {
+        df.name = deparse(substitute(df))
+        cols = colnames(df[, sapply(df, is.factor)])
+    }
+    # Create plot(s)
+    for (i in cols){
+        if (missing(df.num)){
+            plot(df[, i],
+                 main = paste(df.name, "$", i, sep = ""),
+                 ylim = c(0, 1.15*max(summary(df[, i]))),
                  ylab = "Frequency")
+        } else if (!class(df.num) %in% c("numeric", "integer")){
+            stop("Please supply a numeric variable to df.num")
+        } else {
+            num = unlist(strsplit(deparse(substitute(df.num)),
+                                  split = "$", fixed = T))[2]
+            barplot(table(df[, num], df[, i]),
+                    main = paste(df.name, "$", i, " by ", 
+                                 deparse(substitute(df.num)), sep = ""),
+                    ylim = c(0, 1.15*max(table(df[, i], df[, num]))),
+                    ylab = "Frequency", beside = T)
         }
     }
 }
@@ -392,15 +502,35 @@ fac.barplot = function(df, list, var = NULL, cat = F){
 # fac.mosaic()
 #--------------------------------------
 # Function to create mosaic plots of factor variables
-fac.mosaic = function(df, list, var){
+fac.mosaic = function(df.fac, df.fac.cn){
     require(RColorBrewer)
-    for (fac in list){
-        plot(df[, var], df[, fac],
-             col = brewer.pal(nlevels(df[, fac]), "Spectral"),
-             main = paste(data.name, var," versus ",
-                          data.name, fac, sep = ""),
-             xlab = paste(data.name, var, sep = ""),
-             ylab = paste(data.name, fac, sep = ""))
+    # Check df.fac is factor
+    if (!class(df.fac) %in% c("factor")){
+        stop("Please supply a factor variable to df.fac")
+    }
+    # Assign data.frame and name
+    temp = unlist(strsplit(deparse(substitute(df.fac)),
+                           split = "$", fixed = T))
+    df = eval(as.name(paste(temp[1])))
+    df.name = temp[1]
+    fac = temp[2]
+    # Check if df.fac.cn is missing or named (and class if named)
+    if (missing(df.fac.cn)){
+        cols = colnames(df[, sapply(df, is.factor)])
+    } else if (!class(df.fac.cn) %in% c("factor")){
+        stop("Please supply a factor variable to df.fac.cn")
+    } else {
+        cols = unlist(strsplit(deparse(substitute(df.fac.cn)),
+                               split = "$", fixed = T))[2]
+    }
+    # Create plot(s)
+    for (i in cols){
+        plot(df[, fac], df[, i],
+             col = brewer.pal(nlevels(df[, i]), "Spectral"),
+             main = paste(df.name, "$", fac," versus ",
+                          df.name, "$", i, sep = ""),
+             xlab = paste(df.name, "$", fac, sep = ""),
+             ylab = paste(df.name, "$", i, sep = ""))
     }
 }
 
@@ -412,23 +542,27 @@ fac.mosaic = function(df, list, var){
 # fac.freq()
 #--------------------------------------
 # Frequency of occurence split by named factor for factor variables
-fac.freq = function(df.fac, df.cn, cat = T){
+fac.freq = function(df.fac, df.fac.cn, cat = T){
+    table.results = data.frame()
+    # Check df.fac is factor
     if (!class(df.fac) %in% c("factor")){
         stop("Please supply a factor variable to df.fac")
     }
-    table.results = data.frame()
-    df = eval(as.name(paste(unlist(strsplit(deparse(substitute(df.fac)),
-                                            split = "$", fixed = T))[1])))
-    fac = unlist(strsplit(deparse(substitute(df.fac)),
-                          split = "$", fixed = T))[2]
-    if (missing(df.cn)){
+    # Assign data.frame and name
+    temp = unlist(strsplit(deparse(substitute(df.fac)),
+                           split = "$", fixed = T))
+    df = eval(as.name(paste(temp[1])))
+    fac = temp[2]
+    # Check if df.fac.cn is missing or named (and class if named)
+    if (missing(df.fac.cn)){
         cols = colnames(df[, sapply(df, is.factor)])
-    } else if (!class(df.cn) %in% c("factor")){
-        stop("Please supply a factor variable to df.cn")
+    } else if (!class(df.fac.cn) %in% c("factor")){
+        stop("Please supply a factor variable to df.fac.cn")
     } else {
-        cols = unlist(strsplit(deparse(substitute(df.cn)),
+        cols = unlist(strsplit(deparse(substitute(df.fac.cn)),
                                split = "$", fixed = T))[2]
     }
+    # Factor splits
     if (cat){
         for (i in cols){
             name.var = rep(paste(i), each = nlevels(df[, fac]))
@@ -443,13 +577,14 @@ fac.freq = function(df.fac, df.cn, cat = T){
             colnames(table.results)[1] = "Variable"
             colnames(table.results)[2] = "Split On"
             colnames(table.results)[3] = "Levels"
-            if (missing(df.cn)){
+            if (missing(df.fac.cn)){
                 print(table.results)
             } else {
                 return(table.results)
             }
         }
     }
+    # Factor counts and frequencies
     if (!cat){
         name.var = rep(paste(fac), each = 2)
         name.type = c("Count", "Percent")
@@ -470,7 +605,19 @@ fac.freq = function(df.fac, df.cn, cat = T){
 #--------------------------------------
 # Function to create indicator variables from factor variable levels
 fac.flag = function(df){
-    cols = colnames(df[, sapply(df, is.factor)])
+    # Check for data.frame or attribute
+    if (grepl("$", deparse(substitute(df)), fixed = T)){
+        if (!class(df) %in% c("factor")){
+            stop("Please supply a factor variable to df")
+        }
+        temp = unlist(strsplit(deparse(substitute(df)),
+                               split = "$", fixed = T))
+        df = eval(as.name(paste(temp[1])))
+        cols = temp[2]
+    } else {
+        cols = colnames(df[, sapply(df, is.factor)])
+    }
+    # Apply function
     for (i in cols){
         for (level in unique(df[, i])){
             df[paste(i, level, sep = "_")] =
